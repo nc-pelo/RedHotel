01b8dedf3310d49044ee8da5f8a2defb8131d2ab
NCLAN\pelo <pelo@netcompany.com>
2019-04-23 09:58:07 +0200

    Initial commit

---
+++ src/lobby/Main.java
@@ -0,0 +1,19 @@
+package lobby;
+
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        /*
+        JOptionPane.showConfirmDialog(null, "Hello from me");
+        JOptionPane.showInputDialog("How are you?");
+        JOptionPane.showMessageDialog(null , "Remember the birds!");
+        */
+
+        
+
+    }
+
+}
